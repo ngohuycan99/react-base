ffae80f5da360705e91b5885434bf55100853ce2
cannh <cannh@vnext.com.vn>
2022-07-06 17:33:20 +0700

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <StyledP className="text-red-500">
+          Edit <code>src/AStyledPp.tsx</code> and save to reload.
+        </StyledP>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -24,3 +26,7 @@ function App() {
 }
 
 export default App;
+
+const StyledP = styled.p`
+  ${tw`text-xs`}
+`;
